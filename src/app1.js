ffb5ba404a39bc983e1d81d58befa914e2956238
kiki <772765729@qq.com>
2019-11-26 17:54:10 +0800

    vue重构

--- src/app1.js
+++ src/app1.js
@@ -1,53 +1,36 @@
-import $ from "jquery";
 import "./app1.css";
-import Model from "./base/Model.js";
-import View from "./base/View";
-
-const m = new Model({
-  data: {
-    n: parseInt(localStorage.getItem("app1.n")) || 0
-  },
-  update(data) {
-    Object.assign(m.data, data);
-    this.trigger("m_updated");
-    localStorage.setItem("app1.n", m.data.n);
-  }
-});
+import Vue from "vue";
 
 const init = el => {
-  const view = new View({
+  new Vue({
     el: el,
-    data: m.data,
-    html: `
-    <div >
+    data: {
+      n: parseInt(localStorage.getItem("app1.n")) || 0
+    },
+    methods: {
+      add() {
+        this.n += 1;
+      },
+      minus() {
+        this.n -= 1;
+      }
+    },
+    watch: {
+      n() {
+        localStorage.setItem("app1.n", this.n);
+      }
+    },
+    template: `
+    <section >
       <div class="result">
         <span id="number">{{n}}</span>
       </div>
       <div class="actions">
-        <button id="add">+1</button>
-        <button id="minus">-1</button>
+        <button @click="add">+1</button>
+        <button @click="minus">-1</button>
       </div>
-    </div>
-  `,
-
-    render(data) {
-      const n = data.n;
-      if (this.el.children.length !== 0) {
-        this.el.empty();
-      }
-      $(this.html.replace("{{n}}", n)).appendTo($(this.el));
-    },
-
-    events: {
-      "click #add": "add",
-      "click #minus": "minus"
-    },
-    add() {
-      m.update({ n: m.data.n + 1 });
-    },
-    minus() {
-      m.update({ n: m.data.n - 1 });
-    }
+    </section>
+  `
   });
 };
 
--- src/app2.js
+++ src/app2.js
@@ -1,51 +1,30 @@
-import $ from "jquery";
 import "./app2.css";
-import Model from "./base/Model";
-import View from "./base/View";
-
-const m = new Model({
-  data: {
-    index: parseInt(localStorage.getItem("app2.index")) || 0
-  },
-  update(data) {
-    Object.assign(m.data, data);
-    this.trigger("m_updated");
-    localStorage.setItem("app2.index", m.data.index);
-  }
-});
+import Vue from "vue";
 
 const init = el => {
-  const view = new View({
+  new Vue({
     el: el,
-    data: m.data,
-    html: index => {
-      return `
-      <section id="app2">
+    data: {
+      index: parseInt(localStorage.getItem("app2.index")) || 0
+    },
+    watch: {
+      index() {
+        localStorage.setItem("app2.index", this.index);
+      }
+    },
+    template: `
+    <section id="app2">
       <ol class="tab-bar">
-        <li class='${index === 0 ? "selected" : ""}' data-index='0'>1</li>
-        <li class='${index === 1 ? "selected" : ""}' data-index='1'>2</li>
+        <li :class="index === 0 ? 'selected':''"
+            @click='index=0'>1</li>
+        <li :class="index === 1 ? 'selected':''"
+            @click='index=1'>2</li>
       </ol>
       <ol class="tab-content">
-        <li class='${index === 0 ? "active" : ""}'>hello</li>
-        <li class='${index === 1 ? "active" : ""}'>world</li>
+        <li :class="index === 0 ? 'active':''">hello</li>
+        <li :class="index === 1 ? 'active':''">world</li>
       </ol>
-    </section>
-    `;
-    },
-    render(data) {
-      const index = data.index;
-      if (this.el.children.length !== 0) {
-        this.el.empty();
-      }
-      $(this.html(index)).appendTo($(this.el));
-    },
-    events: {
-      "click .tab-bar li": "x"
-    },
-    x(e) {
-      const index = parseInt(e.currentTarget.dataset.index);
-      m.update({ index: index });
-    }
+    </section>`
   });
 };
 
